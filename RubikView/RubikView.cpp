1dd61dbb47da787bd0ff38f347ff21bb00475819
Vincent Zalzal <vincent.zalzal@gmail.com>
2016-12-17 19:46:56 -0500

    Formatting: - Tabs instead of spaces - 8-space indents - Braces not indented - C++ comments - Vars start with an uppercase

--- RubikView/RubikView.cpp
+++ RubikView/RubikView.cpp
@@ -2,37 +2,35 @@
 #include <GLFW/glfw3.h>
 
 int main(void)
-   {
-   GLFWwindow* window;
+{
+	// Initialize the library
+	if (!glfwInit())
+		return -1;
 
-   /* Initialize the library */
-   if (!glfwInit())
-      return -1;
+	// Create a windowed mode window and its OpenGL context
+	GLFWwindow* Window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!Window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-   /* Create a windowed mode window and its OpenGL context */
-   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-   if (!window)
-      {
-      glfwTerminate();
-      return -1;
-      }
+	// Make the window's context current
+	glfwMakeContextCurrent(Window);
 
-   /* Make the window's context current */
-   glfwMakeContextCurrent(window);
+	// Loop until the user closes the window
+	while (!glfwWindowShouldClose(Window))
+	{
+		// Render here
+		glClear(GL_COLOR_BUFFER_BIT);
 
-   /* Loop until the user closes the window */
-   while (!glfwWindowShouldClose(window))
-      {
-      /* Render here */
-      glClear(GL_COLOR_BUFFER_BIT);
+		// Swap front and back buffers
+		glfwSwapBuffers(Window);
 
-      /* Swap front and back buffers */
-      glfwSwapBuffers(window);
+		// Poll for and process events
+		glfwPollEvents();
+	}
 
-      /* Poll for and process events */
-      glfwPollEvents();
-      }
-
-   glfwTerminate();
-   return 0;
-   }
+	glfwTerminate();
+	return 0;
+}

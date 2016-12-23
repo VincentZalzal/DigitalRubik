feccf99aa8db13dc1914a6663383d30f14f0b9bd
Vincent Zalzal <vincent.zalzal@gmail.com>
2016-12-23 18:39:09 -0500

    - Added Rotation "enum". - Added named constants for clarity. - Added typedef for facelet index, for clarity. - Added some comments.

--- Cube/cube.cpp
+++ Cube/cube.cpp
@@ -1,25 +1,34 @@
 #include "cube.h"
 
-Facelet::Type g_Facelets[54];
-
-struct SRotation
+namespace 
 {
-	uint8_t Side[12];
-	uint8_t Front[8];
-	uint8_t Fixed;
+Facelet::Type g_Facelets[Cube::NumFacelets]; // Global cube state.
+
+const uint8_t NumSideFacelets     = 12; // Number of facelets on the side of a turning face.
+const uint8_t NumFrontFacelets    =  8; // Number of facelets on the front of a turning face, excluding the center.
+const uint8_t NumAffectedFacelets = NumSideFacelets + NumFrontFacelets + 1; // Number of facelets of a face.
+
+typedef uint8_t FaceletIndex;
+
+struct SRotation // contains NumAffectedFacelets facelets.
+{
+	FaceletIndex Side[NumSideFacelets];
+	FaceletIndex Front[NumFrontFacelets];
+	FaceletIndex Fixed;
 };
 
+// These are in the same order as namespace Rotation.
 const SRotation Rot[6] =
 {
-	{{9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42}, {2, 5, 8, 7, 6, 3, 0, 1}, 4},
-	{{47, 46, 45, 20, 19, 18, 8, 5, 2, 42, 43, 44}, {11, 14, 17, 16, 15, 12, 9, 10}, 13},
-	{{51, 52, 53, 29, 28, 27, 6, 7, 8, 15, 16, 17}, {20, 23, 26, 25, 24, 21, 18, 19}, 22},
-	{{53, 50, 47, 38, 37, 36, 0, 3, 6, 24, 25, 26}, {29, 32, 35, 34, 33, 30, 27, 28}, 31},
-	{{47, 46, 45, 11, 10, 9, 2, 1, 0, 33, 34, 35}, {38, 41, 44, 43, 42, 39, 36, 37}, 40},
-	{{35, 32, 29, 26, 23, 20, 17, 14, 11, 44, 41, 38}, {47, 50, 53, 52, 51, 48, 45, 46}, 49}
+	{{9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42}, {2, 5, 8, 7, 6, 3, 0, 1}, 4},			// top
+	{{47, 46, 45, 20, 19, 18, 8, 5, 2, 42, 43, 44}, {11, 14, 17, 16, 15, 12, 9, 10}, 13},		// front
+	{{51, 52, 53, 29, 28, 27, 6, 7, 8, 15, 16, 17}, {20, 23, 26, 25, 24, 21, 18, 19}, 22},		// right
+	{{53, 50, 47, 38, 37, 36, 0, 3, 6, 24, 25, 26}, {29, 32, 35, 34, 33, 30, 27, 28}, 31},		// back
+	{{47, 46, 45, 11, 10, 9, 2, 1, 0, 33, 34, 35}, {38, 41, 44, 43, 42, 39, 36, 37}, 40},		// left
+	{{35, 32, 29, 26, 23, 20, 17, 14, 11, 44, 41, 38}, {47, 50, 53, 52, 51, 48, 45, 46}, 49}	// bottom
 };
 
-static void RotateImpl(const uint8_t* Indices, uint8_t NumFacelets)
+static void RotateImpl(const FaceletIndex* Indices, uint8_t NumFacelets)
 {
 
 	Facelet::Type Temp = g_Facelets[Indices[0]];
@@ -28,6 +37,7 @@ static void RotateImpl(const uint8_t* Indices, uint8_t NumFacelets)
 		g_Facelets[Indices[i]] = g_Facelets[Indices[i + 1]];
 	}
 	g_Facelets[Indices[NumFacelets - 1]] = Temp;
+}
 }
 
 namespace Cube
@@ -43,41 +53,41 @@ void Reset()
 	// Numerical order of colors in Facelet match initialization order.
 	Facelet::Type* pFacelet = g_Facelets;
 	for (Facelet::Type Color = Facelet::White; Color < Facelet::Unused; ++Color)
-		for (uint8_t i = 0; i < 9; ++i)
+		for (uint8_t i = 0; i < NumFaceletsPerFace; ++i)
 			*pFacelet++ = Color;
 }
 
-void Brighten(uint8_t Side)
+void Brighten(Rotation::Type Face)
 {
-	const uint8_t* Indices = &Rot[Side].Side[0];
-	for (uint8_t i = 0; i < 21; ++i)
+	const FaceletIndex* Indices = &Rot[Face].Side[0];
+	for (uint8_t i = 0; i < NumAffectedFacelets; ++i)
 		g_Facelets[Indices[i]] |= Facelet::Bright;
 }
 
-void RotateSide(uint8_t Side)
+void RotateSide(Rotation::Type Face)
 {
-	const uint8_t* Indices = &Rot[Side].Side[0];
-	RotateImpl(Indices, 12);
+	const FaceletIndex* Indices = &Rot[Face].Side[0];
+	RotateImpl(Indices, NumSideFacelets);
 }
 
-void RotateFront(uint8_t Side)
+void RotateFront(Rotation::Type Face)
 {
-	const uint8_t* Indices = &Rot[Side].Front[0];
-	RotateImpl(Indices, 8);
+	const FaceletIndex* Indices = &Rot[Face].Front[0];
+	RotateImpl(Indices, NumFrontFacelets);
 }
 
-void Rotate(uint8_t Side)
+void Rotate(Rotation::Type Face)
 {
-	RotateSide(Side);
-	RotateSide(Side);
-	RotateSide(Side);
-	RotateFront(Side);
-	RotateFront(Side);
+	RotateSide(Face);
+	RotateSide(Face);
+	RotateSide(Face);
+	RotateFront(Face);
+	RotateFront(Face);
 }
 
 void DimAll()
 {
-	for (uint8_t i = 0; i < 54; ++i)
+	for (FaceletIndex i = 0; i < NumFacelets; ++i)
 		g_Facelets[i] &= ~Facelet::Bright;
 }
 
--- Cube/cube.h
+++ Cube/cube.h
@@ -9,16 +9,16 @@ typedef uint8_t Type;
 const Type Black  = 0;
 
 // Order of colors maps to initialization LED order
-const Type White  = 1;
-const Type Red    = 2;
-const Type Blue   = 3;
-const Type Orange = 4;
-const Type Green  = 5;
-const Type Yellow = 6;
+const Type White  = 1;	// At reset: top
+const Type Red    = 2;	// At reset: front
+const Type Blue   = 3;	// At reset: right
+const Type Orange = 4;	// At reset: back
+const Type Green  = 5;	// At reset: left
+const Type Yellow = 6;	// At reset: bottom
 
 const Type Unused = 7;
 
-const Type Bright = 8; // additive bit
+const Type Bright = 8;	// additive bit
 }
 
 struct SColor
@@ -27,7 +27,8 @@ struct SColor
 };
 
 // This color LUT must be in SRAM for fast access during LED update.
-// Warning: order is GRB
+// Indices map to the Facelet values.
+// Warning: color order is GRB.
 const SColor Colors[15] =
 {
 	{  0,   0,   0}, // Black 
@@ -47,13 +48,31 @@ const SColor Colors[15] =
 	{255, 255,   0}  // Bright Yellow
 };
 
+namespace Rotation
+{
+typedef uint8_t Type;
+
+// By default, clockwise rotation
+const Type Top    = 0;
+const Type Front  = 1;
+const Type Right  = 2;
+const Type Back   = 3;
+const Type Left   = 4;
+const Type Bottom = 5;
+const Type CCW    = 6; // additive, means counter-clockwise
+}
+
 namespace Cube
 {
-const Facelet::Type* GetFacelets(); // Get pointer to 54 facelets, in LED order
-void Reset(); // Reset cube to solved state.
-void Brighten(uint8_t Side);
-void RotateSide(uint8_t Side);
-void RotateFront(uint8_t Side);
-void Rotate(uint8_t Side);
+const uint8_t NumFaceletsPerFace = 9;
+const uint8_t NumFaces = 6;
+const uint8_t NumFacelets = NumFaceletsPerFace * NumFaces;
+
+const Facelet::Type* GetFacelets();	// Get pointer to 54 facelets, in LED order
+void Reset();				// Reset cube to solved state.
+void Brighten(Rotation::Type Face);
+void RotateSide(Rotation::Type Face);
+void RotateFront(Rotation::Type Face);
+void Rotate(Rotation::Type Face);
 void DimAll();
 }

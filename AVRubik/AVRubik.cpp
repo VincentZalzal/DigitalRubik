bef2c758f652ae1e6a3c3538ebafae24885799c3
Vincent Zalzal <vincent.zalzal@gmail.com>
2017-01-01 16:36:23 -0500

    Added Atmel Studio 6.2 solution and project. For now, it uses an ATmega328.

---
+++ AVRubik/AVRubik.cpp
@@ -0,0 +1,26 @@
+/*
+ * AVRubik.cpp
+ *
+ * Created: 2017-01-01 16:14:44
+ *  Author: Vincent
+ */ 
+
+
+#include <avr/io.h>
+
+#define F_CPU 8000000UL
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRD |= 0x01;
+    PORTD = 0;
+	DDRC |= 0x20;
+	while(1)
+	{
+		PORTC |= 0x20;
+		_delay_ms(2000);
+		PORTC &= ~0x20;
+		_delay_ms(2000);
+	}
+}

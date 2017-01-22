f429070b3cd5d27c93d2871e13852ebe90f025c0
Vincent Zalzal <vincent.zalzal@gmail.com>
2017-01-21 21:52:16 -0500

    Inverted preprocessor define logic so that no define is necessary on AVR.

--- Cube/config.h
+++ Cube/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Flash memory handling (for AVR only)
-#ifndef USE_AVR
+#ifdef USE_SIMULATOR
 	// This is needed for non-AVR platforms.
 	#define PROGMEM
 	#define pgm_read_byte(Addr)	(*(Addr))
@@ -10,17 +10,17 @@
 #endif
 
 // Asserts (disabled on AVR)
-#ifdef USE_AVR
+#ifdef USE_SIMULATOR
+	#include <cassert>
+#else
 	// Disable asserts.
 	#define assert(Condition) ((void)0)
-#else
-	#include <cassert>
 #endif
 
 // Static asserts
-// If you don't want static asserts, define USE_STATIC_ASSERT to 0.
+// If you want static asserts, define USE_STATIC_ASSERT to 1.
 #ifndef USE_STATIC_ASSERT
-	#define USE_STATIC_ASSERT 1
+	#define USE_STATIC_ASSERT 0
 #endif
 
 #if USE_STATIC_ASSERT

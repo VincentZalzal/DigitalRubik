21cef1854425b10dcd693079c0ff0b799bb38813
Vincent Zalzal <vincent.zalzal@gmail.com>
2017-01-22 20:50:34 -0500

    Added outline of main AVR program.

--- AVRubik/AVRubik.cpp
+++ AVRubik/AVRubik.cpp
@@ -1,31 +1,83 @@
-/*
- * AVRubik.cpp
- *
- * Created: 2017-01-01 16:14:44
- *  Author: Vincent
- */
-
 #include "avr_specific.h"
 #include "rings.h"
 #include "leds.h"
+#include "../Cube/rand8.h"
 #include "../Cube/cube.h"
+
+#define NUM_SCRAMBLE_ROTATIONS		30
+#define ROTATION_DELAY_MS		150
+#define VICTORY_ANIMATION_DELAY_MS	400
 
 void Init()
 {
 	Rings::Init();
 	Leds::Init();
 	Cube::Reset();
+	Rand8::Seed(42); // TODO: seed RNG using ADC
+	// TODO: disable unused components to reduce power consumption
 }
 
 int main(void)
 {
 	Init();
 
-	Cube::Scramble(30);
+	Cube::Scramble(NUM_SCRAMBLE_ROTATIONS);
 	Leds::Update();
 
-	while(1)
+	while(1) // TODO: !Cube::IsSolved()
 	{
 		Rings::Read();
+		bool CubeHasChanged = true; // TODO: Controls::UpdateCube();
+		if (CubeHasChanged)
+			Leds::Update();
+		
+		// TODO: Controls::ActionType Action = DetermineAction();
+		uint8_t Action = Rotation::Top;
+		
+		// TODO
+		//if (Action == Undo)
+		//{
+		//	
+		//}
+		//else if (Action == Menu)
+		//{
+		//	
+		//}
+		//else
+		{
+			// Perform the rotation animation.
+			Cube::RotateSide(Action);
+			Cube::RotateFront(Action);
+			Leds::Update();
+			_delay_ms(ROTATION_DELAY_MS);
+			
+			Cube::RotateSide(Action);
+			Leds::Update();
+			_delay_ms(ROTATION_DELAY_MS);
+
+			Cube::RotateSide(Action);
+			Cube::RotateFront(Action);
+			Leds::Update();
+			_delay_ms(ROTATION_DELAY_MS);
+			
+			// TODO: Rings::Reset();
+			// TODO: Controls::Reset();
+			Cube::DimAll();
+			Leds::Update();
+		}
+	}
+	
+	while(1)
+	{
+		// Perform victory animation forever.
+		// TODO: Cube::BrightenRandom();
+		//Cube::DimAll();
+		//for (uint8_t i = 0; i < 25; ++i)
+		//{
+		//	uint8_t CurFacelet = Rand8::Get(0, 53);
+		//	Cube::BrightenFacelet(CurFacelet);
+		//}
+		Leds::Update();
+		_delay_ms(VICTORY_ANIMATION_DELAY_MS);
 	}
 }
